6e6aa10d79072620ebd4539fd1803300697181ff
saikiran792-code <saikirans428@gmail.com>
2026-06-20 14:54:57 +0530

    Process of the Jwt token generator is done

--- notes/src/main/java/com/secure/notes/service/JwtTokenService.java
+++ notes/src/main/java/com/secure/notes/service/JwtTokenService.java
@@ -1,23 +1,36 @@
 package com.secure.notes.service;
 
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Service;
 
 import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+import java.security.Key;
 import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
+
+
 @Service
 public class JwtTokenService {
+    private String secretKey="";
 
     public JwtTokenService() throws NoSuchAlgorithmException {
-        KeyGenerator key=KeyGenerator.getInstance("HmacSHA256");
+        KeyGenerator keyGen=KeyGenerator.getInstance("HmacSHA256");
+        SecretKey sk=keyGen.generateKey();
+        secretKey=Base64.getEncoder().encodeToString(sk.getEncoded());
 
     }
 
-
+    private Key getKey(){
+        byte[] keyBytes= Decoders.BASE64.decode(secretKey);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
     public String generatejwttoken(String username) {
         Map<String,Object> claims=new HashMap<>();
 
@@ -26,13 +39,9 @@ public class JwtTokenService {
                 .add(claims)
                 .subject(username)
                 .issuedAt(new Date(System.currentTimeMillis()))
-                .expiration(new Date(System.currentTimeMillis() * 60 *60 *60))
+                .expiration(new Date(System.currentTimeMillis() + 60 *60 *60))
                 .and()
-//                .signWith(getKey())
-                .compact()
-
-
-
-        return "";
+                .signWith(getKey())
+                .compact();
     }
 }
